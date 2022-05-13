1583d4e17a661caff363dacd7c84913e2cba3918
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-13 15:43:13 +0200

    seed file with logic updated

---
+++ utils/data.js
@@ -0,0 +1,56 @@
+const data = [
+    {
+        brand: "Nike",
+        genre: "Man",
+        category: "Shoes",
+        title: "Nike React Infinity Run Flyknit 3",
+        price: 160,
+        img: `https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5a28e397-d083-4398-b0d9-a9911ae22018/react-infinity-run-flyknit-3-zapatillas-de-running-carretera-Pp5hlk.png`,
+        description:
+            "Nuestras zapatillas más probadas, diseñadas para ayudarte a superar tras tus metas más altas metas de running. Las Nike React Infinity Run 3 ofrecen suavidad y estabilidad con una pisada agradable que te llevará por cualquier ruta, larga o corta. La parte superior transpirable está diseñada para ofrecer contención, sin perder flexibilidad. Incluso hemos añadido más amortiguación en el talón y el tobillo para proporcionar una sensación de sujeción. No dejes de correr, lo tiene todo.",
+        units: 10,
+        sizes: "39, 40, 41, 42, 43, 44",
+        value: 0,
+    },
+    {
+        brand: "Nike",
+        genre: "Man",
+        category: "Tshirts",
+        title: "Nike Dri-FIT",
+        price: 29,
+        img: `https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/kxjgqmbim9zxricso57p/dri-fit-camiseta-de-entrenamiento-LPg5Tp.png`,
+        description:
+            "La camiseta Nike Dri-FIT cuenta con un suave diseño de alto rendimiento con capilarización del sudor para ofrecer comodidad y una gran libertad de movimiento durante tus entrenamientos.",
+        units: 10,
+        sizes: "S, M, L, XL",
+        value: 0,
+    },
+    {
+        brand: "Nike",
+        genre: "Woman",
+        category: "Shoes",
+        title: "Nike Zoom Alphafly Next Nature",
+        price: 299,
+        img: `https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/04993055-6221-4a74-8ff2-bd37b481c057/zoom-alphafly-next-nature-zapatillas-de-competicion-carretera-3mk9g2.png`,
+        description:
+            "Dalo todo para llegar a la meta con el siguiente paso en nuestro camino de sostenibilidad.Hemos confeccionado las ultraligeras Nike Air Zoom Alphafly Next Nature con, al menos, un 50 % de materiales reciclados por peso y que no reducen el rendimiento.Se han diseñado para mejorar tus récords personales con un diseño seguro, transpirable y tan ligero que ofrece la sensación de volar.Has entrenado duro, ponte las zapatillas y dalo todo con unas zapatillas confeccionadas con residuos.",
+        units: 10,
+        sizes: "35, 36, 37, 38, 39, 40, 41",
+        value: 0,
+    },
+    {
+        brand: "Nike",
+        genre: "Woman",
+        category: "Tshirts",
+        title: "Nike Sportswear",
+        price: 20,
+        img: `https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/80afc109-1fdf-4265-8ab4-c0319f089466/sportswear-camiseta-club-7t45WK.png`,
+        description:
+            "Es un básico por muchas razones: la camiseta Nike Sportswear Club presenta un ajuste clásico y está confeccionada con un tejido de punto de algodón suave, perfecto para el día a día.",
+        units: 10,
+        sizes: "XS, S, M, L",
+        value: 0,
+    },
+];
+
+export { data };
--- utils/seedShop.js
+++ utils/seedShop.js
@@ -1,1 +1,158 @@
-console.log("gol");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { Category } from "../api/models/categories.schema.js";
+import { Product } from "../api/models/product.schema.js";
+import { Genre } from "../api/models/genre.schema.js";
+import { Shop } from "../api/models/shop.schema.js";
+import { data } from "./data.js";
+
+dotenv.config();
+const DB_URL = process.env.DB_URL;
+
+const products = data;
+
+const shop = [
+    {
+        genres: [],
+        lastBuys: [],
+        mostValuated: [],
+        mostCommented: [],
+    },
+];
+
+const genres = [
+    {
+        genre: "Man",
+        categories: [],
+    },
+    {
+        genre: "Woman",
+        categories: [],
+    },
+];
+
+const categories = [
+    {
+        name: "Shoes",
+        genre: "Man",
+        products: [],
+    },
+    {
+        name: "Tshirts",
+        genre: "Man",
+        products: [],
+    },
+    {
+        name: "Shoes",
+        genre: "Woman",
+        products: [],
+    },
+    {
+        name: "Tshirts",
+        genre: "Woman",
+        products: [],
+    },
+];
+
+const shopDoc = shop.map((shop) => new Shop(shop));
+
+const genresDoc = genres.map((genre) => new Genre(genre));
+
+const categoriesDoc = categories.map((category) => new Category(category));
+
+const productsDoc = products.map((product) => new Product(product));
+
+const creationSeed = mongoose
+    .connect(
+        "mongodb+srv://vlcoscar8:sMZl71f0NTWiHJnA@cluster0.sod8s.mongodb.net/E-commerce",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(async () => {
+        await createCollections();
+        await insertGenresOnShop();
+        await insertCategoriesOnGenre();
+        await insertProductOnCategories();
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    .finally(() => {
+        console.log("Seed created successfully");
+    });
+
+const createCollections = async () => {
+    await Shop.collection.drop();
+    await Shop.insertMany(shopDoc);
+
+    await Genre.collection.drop();
+    await Genre.insertMany(genresDoc);
+
+    await Category.collection.drop();
+    await Category.insertMany(categoriesDoc);
+
+    await Product.collection.drop();
+    await Product.insertMany(productsDoc);
+};
+
+const insertGenresOnShop = async () => {
+    const genres = await Genre.find();
+    const shop = await Shop.find();
+
+    genres.forEach(
+        async (genre) =>
+            await Shop.findByIdAndUpdate(shop[0].id, {
+                $push: {
+                    genre: genre,
+                },
+            })
+    );
+};
+
+const insertCategoriesOnGenre = async () => {
+    const categories = await Category.find();
+    const manCategories = categories.filter((cat) => cat.genre === "Man");
+    const womanCategories = categories.filter((cat) => cat.genre === "Woman");
+
+    manCategories.forEach(
+        async (cat) =>
+            await Genre.findOneAndUpdate(
+                { genre: cat.genre },
+                {
+                    $push: {
+                        categories: cat,
+                    },
+                }
+            )
+    );
+
+    womanCategories.forEach(
+        async (cat) =>
+            await Genre.findOneAndUpdate(
+                { genre: cat.genre },
+                {
+                    $push: {
+                        categories: cat,
+                    },
+                }
+            )
+    );
+};
+
+const insertProductOnCategories = async () => {
+    const products = await Product.find();
+
+    products.forEach(
+        async (pro) =>
+            await Category.findOneAndUpdate(
+                { genre: pro.genre, name: pro.category },
+                {
+                    $push: {
+                        products: pro,
+                    },
+                }
+            )
+    );
+};

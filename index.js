b7c5f56411dacab2271b775c725f8f4398877e55
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-14 18:48:36 +0200

    deploy page updated

--- index.js
+++ index.js
@@ -44,7 +44,14 @@ server.use(
 
 // Router
 server.all("/", (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.send(`
+    <h1>Ecommerce api about sneakers done by Oscar Perez</h1>
+    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5a28e397-d083-4398-b0d9-a9911ae22018/react-infinity-run-flyknit-3-zapatillas-de-running-carretera-Pp5hlk.png"/>
+
+    <h2>Endpoints:</h2>
+
+    <a href="/Home">Home</a>
+    `);
 });
 server.use("/home", homeRouter);
 server.use("/products", productListRouter);

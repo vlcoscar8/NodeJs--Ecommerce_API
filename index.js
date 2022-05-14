6de0311321b42ea3bcf45a9bc85fc74f56eccc08
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-14 19:46:33 +0200

    index change position of router

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const DB_URL = process.env.DB_URL;
 
 // Configuration
 server.set("secretKey", "nodeRestApi");
-server.use("/", router);
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(
@@ -43,39 +42,6 @@ server.use(
         }),
     })
 );
-
-// Router
-server.all("/", (req, res) => {
-    res.send(`
-    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-        <h1>Ecommerce api about sneakers done by Oscar Perez</h1>
-        <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5a28e397-d083-4398-b0d9-a9911ae22018/react-infinity-run-flyknit-3-zapatillas-de-running-carretera-Pp5hlk.png" style="width: 250px"/>
-
-        <h2>Documentation:</h2>
-        <a href="/api-doc">Sneakers Ecommerce api documentation</a>
-
-        <h3>Git hub repository</h3>
-        <a href="https://github.com/vlcoscar8/Ecommerce_API.git" target="_blank">Sneakers Ecommerce api code</a>
-    </div>
-    `);
-});
-server.use("/home", homeRouter);
-server.use("/products", productListRouter);
-server.use("/product", productDetailRouter);
-server.use("/user", userRouter);
-
-// Errors
-server.use("*", (req, res, next) => {
-    const error = new Error("Route not found");
-    error.status = 404;
-    next(error);
-});
-server.use((error, req, res, next) => {
-    return res
-        .status(error.status || 500)
-        .json(error.message || "Unexpected error");
-});
-
 //Swagger
 const swaggerSpec = {
     definition: {
@@ -93,11 +59,44 @@ const swaggerSpec = {
     apis: [`./api/documentation/*.js`], // The file where the documentation is written
 };
 
+// Router
+server.all("/", (req, res) => {
+    res.send(`
+    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+        <h1>Ecommerce api about sneakers done by Oscar Perez</h1>
+        <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5a28e397-d083-4398-b0d9-a9911ae22018/react-infinity-run-flyknit-3-zapatillas-de-running-carretera-Pp5hlk.png" style="width: 250px"/>
+
+        <h2>Documentation:</h2>
+        <a href="/api-doc">Sneakers Ecommerce api documentation</a>
+
+        <h3>Git hub repository</h3>
+        <a href="https://github.com/vlcoscar8/Ecommerce_API.git" target="_blank">Sneakers Ecommerce api code</a>
+    </div>
+    `);
+});
+
+server.use("/", router);
+server.use("/home", homeRouter);
+server.use("/products", productListRouter);
+server.use("/product", productDetailRouter);
+server.use("/user", userRouter);
 server.use(
     "/api-doc",
     swaggerUi.serve,
     swaggerUi.setup(swaggerJsDoc(swaggerSpec))
 );
+
+// Errors
+server.use("*", (req, res, next) => {
+    const error = new Error("Route not found");
+    error.status = 404;
+    next(error);
+});
+server.use((error, req, res, next) => {
+    return res
+        .status(error.status || 500)
+        .json(error.message || "Unexpected error");
+});
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);

a3bda13059f7fab34313a357fcd91da8ff9690c4
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-23 10:04:32 +0200

    populate user into commentary

--- api/controller/product-detail.controller.js
+++ api/controller/product-detail.controller.js
@@ -1,10 +1,21 @@
+import { Commentary } from "../models/comments.schema.js";
 import { Product } from "../models/product.schema.js";
+import { User } from "../models/user.schema.js";
 
 const getProductDetail = async (req, res, next) => {
     try {
         const { id } = req.params;
 
-        const product = await Product.findOne({ id: id }).populate("comments");
+        const product = await Product.findOne({ id: id }).populate({
+            path: "comments",
+            model: Commentary,
+            populate: [
+                {
+                    path: "user",
+                    model: User,
+                },
+            ],
+        });
 
         res.status(200).json(product);
     } catch (error) {
